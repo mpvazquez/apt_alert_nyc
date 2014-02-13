cc2b958853cd16a9dc55d3823aa54162e19b2f56
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-02-13 15:42:08 -0500

    starting project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AptAlertNyc::Application.config.secret_key_base = 'ffb5be91f0e8b394f5f2cec0a362e0c3abf318c821bf2cfe308786c969d118c6c4a4ab53eb59ad5d2f35b4fc3a380a34cc61269b6e9821883199d91b11449e48'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AptAlertNyc::Application.config.session_store :cookie_store, key: '_apt_alert_nyc_session'
