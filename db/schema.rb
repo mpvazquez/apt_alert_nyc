f74c45a2d2084434d79efdb86489c43f1ed8f5e3
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-02-14 15:20:46 -0500

    created users:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213205242) do
+ActiveRecord::Schema.define(version: 20140214201919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20140213205242) do
   create_table "favorites", force: true do |t|
     t.integer "listing_id"
     t.integer "user_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
